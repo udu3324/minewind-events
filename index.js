295c1e1c6cf2ae9d7aac3b5e7a23cd48def142f1
udu3324 <47045986+udu3324@users.noreply.github.com>
2022-07-12 21:27:56 -0500

    filter fix 2

--- index.js
+++ index.js
@@ -103,101 +103,101 @@ tail.on("line", function (data) {
         sendMessage(channelLabyrinth, `||${pingLabyrinth}||`, false)
         sendEmbed(channelLabyrinth, "Labyrinth", data, "", "ffffff")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingLabyrinth}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingLabyrinth}||`, false)
     } else if (/(^Labyrinth event has started!$)/.test(data)) {
         sendMessage(channelLabyrinth, `||${pingLabyrinth}||`, false)
         sendEmbed(channelLabyrinth, "Labyrinth", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544906917056653/labyrinth_Start.gif", "ffffff")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingLabyrinth}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingLabyrinth}||`, false)
     } else if (/(^Labyrinth event has ended!$)/.test(data)) {
         sendEmbed(channelLabyrinth, "Labyrinth", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544906510217226/labyrinth_End.gif", "ffffff")
 
-        sendMessage(channelRawAlerts, `${data}`)
+        sendMessage(channelRawAlerts, `${data}`, false)
     }
 
     if (/(^Beef Event begins in .* (hour|minute(s|)|seconds)\.$)/.test(data)) {
         sendMessage(channelBeef, `||${pingBeef}||`, false)
         sendEmbed(channelBeef, "Beef", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544951741599834/beef_soon_upscale.png", "001E9A")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingBeef}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingBeef}||`, false)
         if (/(^Beef Event begins in .* seconds\.$)/.test(data)) {
             setTimeout(function () {
                 sendMessage(channelBeef, `||${pingBeef}||`, false)
                 sendEmbed(channelBeef, "Beef", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544952030986270/beef_start_upscale.png", "001E9A")
 
-                sendMessage(channelRawAlerts, `${data}\n||${pingBeef}||`, false)
+                sendMessage(channelRawAlerts, `${data} ||${pingBeef}||`, false)
             }, 10000)
         }
     } else if (/(^Team (aqua|red) wins the beef event!$)/.test(data)) {
         sendEmbed(channelBeef, "Beef", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544951540264990/beef_end_upscale.png", "001E9A")
 
-        sendMessage(channelRawAlerts, `${data}`)
+        sendMessage(channelRawAlerts, `${data}`, false)
     }
 
     if (/^Abyssal event begins in (30|20|10|5|1) (hour|minute(s|)|seconds)\.$/.test(data)) {
         sendMessage(channelAbyssal, `||${pingAbyss}||`, false)
         sendEmbed(channelAbyssal, "Abyssal", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544980174778368/abyss_soon_upscale.png", "0537D6")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingAbyss}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingAbyss}||`, false)
         if (/^Abyssal event begins in 10 seconds\.$/.test(data)) {
             setTimeout(function () {
                 sendMessage(channelAbyssal, `||${pingAbyss}||`, false)
                 sendEmbed(channelAbyssal, "Abyssal", data, "https://media.discordapp.net/attachments/996544779926126692/996544980514521148/abyss_Start.gif", "0537D6")
 
-                sendMessage(channelRawAlerts, `${data}\n||${pingAbyss}||`, false)
+                sendMessage(channelRawAlerts, `${data} ||${pingAbyss}||`, false)
             }, 10000)
         }
     } else if (/^\w{3,16} wins the abyssal event! Poseidon is pleased!$/.test(data)) {
         sendEmbed(channelAbyssal, "Abyssal", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544981223354489/abyss_End.gif", "0537D6")
 
-        sendMessage(channelRawAlerts, `${data}`)
+        sendMessage(channelRawAlerts, `${data}`, false)
     }
 
     if (/^Attack on Giant begins in (30|20|10|5|1) (hour|minute(s|)|seconds)\.$/.test(data)) {
         sendMessage(channelAttackOnGiant, `||${pingAttack}||`, false)
         sendEmbed(channelAttackOnGiant, "Attack on Giant", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544906149498970/giant_soon_upscale.png", "50862a")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingAttack}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingAttack}||`, false)
     } else if (data === "Attack on Giant has begun!") {
         sendMessage(channelAttackOnGiant, `||${pingAttack}||`, false)
         sendEmbed(channelAttackOnGiant, "Attack on Giant", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544953306058812/attack_Start.gif", "50862a")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingAttack}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingAttack}||`, false)
     } else if (data === "Attack on Giant ends!") {
         sendEmbed(channelAttackOnGiant, "Attack on Giant", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544980959109240/attack_End.gif", "50862a")
 
-        sendMessage(channelRawAlerts, `${data}`)
+        sendMessage(channelRawAlerts, `${data}`, false)
     }
 
     if (data === "Snowmen invade the spawn!") {
         sendMessage(channelSnowvasion, `||${pingSnow}||`, false)
         sendEmbed(channelSnowvasion, "Snowvasion", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544907676233768/snowvasion_Start.gif", "ffffff")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingSnow}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingSnow}||`, false)
     } else if (data === "Snowmen melt away!") {
         sendEmbed(channelSnowvasion, "Snowvasion", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544907386814464/snowvasion_End.gif", "ffffff")
 
-        sendMessage(channelRawAlerts, `${data}`)
+        sendMessage(channelRawAlerts, `${data}`, false)
     }
 
     if (/^Fox hunt begins in (30|20|10|5|1) (hour|minute(s|)|seconds)\.$/.test(data)) {
         sendMessage(channelFoxHunt, `||${pingFox}||`, false)
         sendEmbed(channelFoxHunt, "Fox Hunt", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544905616818196/fox_soon_upscale.png", "F5E78F")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingFox}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingFox}||`, false)
     } else if (data === "Fox Hunt has begun!") {
         sendMessage(channelFoxHunt, `||${pingFox}||`, false)
         sendEmbed(channelFoxHunt, "Fox Hunt", data, "https://media.discordapp.net/attachments/996544779926126692/996544905935605791/fox_start_upscale.png", "F5E78F")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingFox}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingFox}||`, false)
     } else if (/^Fox-chan spawned at /.test(data)) {
         sendEmbed(channelFoxHunt, "Fox Hunt", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544904937353339/fox_chanSpawn_upscale.png", "F5E78F")
 
-        sendMessage(channelRawAlerts, `${data}`)
+        sendMessage(channelRawAlerts, `${data}`, false)
     } else if (/^\w{3,16} has slain Fox-chan!$/.test(data)) {
         sendEmbed(channelFoxHunt, "Fox Hunt", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544904719253614/fox_chanDie_upscale.png", "F5E78F")
 
-        sendMessage(channelRawAlerts, `${data}`)
+        sendMessage(channelRawAlerts, `${data}`, false)
     } else if (/^Fox Hunt event ends!$/.test(data)) {
         listenForFoxEnd = true
     } else if (/^[1-3]\) \w{3,16} -- .* foxes$/.test(data) && listenForFoxEnd) {
@@ -205,7 +205,7 @@ tail.on("line", function (data) {
         if (/^3\) \w{3,16} -- .* foxes$/.test(data)) {
             sendEmbed(channelFoxHunt, "Fox Hunt", "Fox Hunt has ended.\n" + foxEndString, "https://cdn.discordapp.com/attachments/996544779926126692/996544905394528407/fox_end_upscale.png", "F5E78F")
 
-            sendMessage(channelRawAlerts, `${"Fox Hunt has ended.\n" + foxEndString}`)
+            sendMessage(channelRawAlerts, `${"Fox Hunt has ended.\n" + foxEndString}`, false)
 
             foxEndString = ""
             listenForFoxEnd = false
@@ -216,13 +216,13 @@ tail.on("line", function (data) {
         sendMessage(channelBait, `||${pingBait}||`, false)
         sendEmbed(channelBait, "Bait", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544951372484688/bait_soon_upscale.png", "001E9A")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingBait}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingBait}||`, false)
         if (/^Bait Event begins in 10 seconds\.$/.test(data)) {
             setTimeout(function () {
                 sendMessage(channelBait, `||${pingBait}||`, false)
                 sendEmbed(channelBait, "Bait", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544953104748594/fish_Start.gif", "001E9A")
 
-                sendMessage(channelRawAlerts, `${data}\n||${pingBait}||`, false)
+                sendMessage(channelRawAlerts, `${data} ||${pingBait}||`, false)
             }, 10000)
         }
     } else if (/^Fishing event ends!$/.test(data)) {
@@ -232,7 +232,7 @@ tail.on("line", function (data) {
         if (/^3\) \w{3,16} -- .* fish$/.test(data)) {
             sendEmbed(channelBait, "Bait", "Bait has ended.\n" + baitEndString, "https://cdn.discordapp.com/attachments/996544779926126692/996544952886636635/fish_End.gif", "001E9A")
 
-            sendMessage(channelRawAlerts, `${"Bait has ended.\n" + baitEndString}`)
+            sendMessage(channelRawAlerts, `${"Bait has ended.\n" + baitEndString}`, false)
 
             baitEndString = ""
             listenForBaitEnd = false
@@ -243,16 +243,16 @@ tail.on("line", function (data) {
         sendMessage(channelCastle, `||${pingCastle}||`, false)
         sendEmbed(channelCastle, "Castle", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544952458825748/castle_soon_upscale.png", "555555")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingCastle}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingCastle}||`, false)
     } else if (data === "Battle for Minewind has begun!") {
         sendMessage(channelCastle, `||${pingCastle}||`, false)
         sendEmbed(channelCastle, "Castle", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544952660131962/castle_start_upscale.png", "555555")
 
-        sendMessage(channelRawAlerts, `${data}\n||${pingCastle}||`, false)
+        sendMessage(channelRawAlerts, `${data} ||${pingCastle}||`, false)
     } else if (/( \(.*\) take the Minewind City from .* \(.*\)!$| \(.*\) hold the Minewind City!$)/.test(data)) {
         sendEmbed(channelCastle, "Castle", data, "https://cdn.discordapp.com/attachments/996544779926126692/996544952261685380/castle_end_upscale.png", "555555")
 
-        sendMessage(channelRawAlerts, `${data}`)
+        sendMessage(channelRawAlerts, `${data}`, false)
     }
 });
 
